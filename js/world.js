760ae01b731e736bf58e49476e13b3e5f76292fc
grabz-dev <38158107+grabz-dev@users.noreply.github.com>
2018-04-08 22:11:57 +0200

    0.3.1

--- js/world.js
+++ js/world.js
@@ -32,24 +32,21 @@ const World = ((window, document) => {
                 let decimal = hours + minutes / 60;
 
                 let r = 20;
-                let g = 20;
+                let g = 0;
                 let b = 20;
 
-                if(decimal >= 22) {
-                    g = 50;
-                }
-                else if(decimal >= 18) {
+                if(decimal >= 18) {
                     g = 100 + (((18 - decimal) * 12.5));
                 }
                 else if(decimal >= 7) {
                     g = 100;
                 }
-                else if(decimal >= 5) {
+                else if(decimal >= 3) {
                     g = 50 + (25 - ((6 - decimal) * 25));
                 }
-                else if(decimal >= 0) {
-                    g = 50;
-                }
+
+                if(g < 30)
+                    g = 30;
 
                 g = Math.round(g);
 
